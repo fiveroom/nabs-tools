1e1bbd89b5e2d8b55313fc5e052a9e5a0a1a5c06
zhou_xi_cheng <lkzipsea@163.com>
2022-02-25 10:56:14 +0800

    feat: 添加网页测试和打包测试

--- html/main.tsx
+++ html/main.tsx
@@ -1,83 +1,89 @@
-import { render } from 'react-dom';
-import { useState } from 'react';
-import { Workbook } from 'exceljs'
-import { getHeadRowMerge } from 'lib/tableHeadTool'
-import { xlsxHeadArr } from 'lib/exceljsExtend'
-import { ExportEx } from 'lib/exceljsExtend'
-import { faker } from '@faker-js/faker'
+import {render} from "react-dom";
+import {useState} from "react";
+import {Workbook} from "exceljs";
+import {getHeadRowMerge} from "lib/tableHeadTool";
+import {xlsxHeadArr} from "lib/exceljsExtend";
+import {ExportEx} from "lib/exceljsExtend";
+import {faker} from "@faker-js/faker";
 function App() {
     const [count, setCount] = useState(0);
 
     let exportUseTool = () => {
         let head: xlsxHeadArr = [
             {
-                prop: 'p1',
-                label: 'p1'
+                prop: "p1",
+                label: "p1",
             },
             {
-                prop: 'p2',
-                label: 'p2'
+                prop: "p2",
+                label: "p2",
             },
             {
-                prop: 'p3',
-                label: 'p3',
+                prop: "p3",
+                label: "p3",
                 children: [
                     {
-                        prop: 'p6',
-                        label: 'p6',
+                        prop: "p6",
+                        label: "p6",
                     },
                     {
-                        prop: 'p7',
-                        label: 'p7',
+                        prop: "p7",
+                        label: "p7",
                         children: [
                             {
-                                prop: 'p8',
-                                label: 'p8',
-                                width: 16
-                            },{
-                                prop: 'p9',
-                                label: 'p9',
-                                width: 60
-                            }
-                        ]
-                    }
-                ]
+                                prop: "p8",
+                                label: "p8",
+                                width: 16,
+                            },
+                            {
+                                prop: "p9",
+                                label: "p9",
+                                width: 60,
+                            },
+                        ],
+                    },
+                ],
             },
             {
-                prop: 'p4',
-                label: 'p4',
+                prop: "p4",
+                label: "p4",
                 children: [
                     {
-                        prop: 'p10',
-                        label: 'p10'
+                        prop: "p10",
+                        label: "p10",
                     },
                     {
-                        prop: 'p11',
-                        label: 'p11'
-                    }
-                ]
+                        prop: "p11",
+                        label: "p11",
+                    },
+                ],
             },
             {
-                prop: 'p5',
-                label: 'p5',
+                prop: "p5",
+                label: "p5",
+                format: value => {
+                    return "p5 format";
+                },
+                align: "center",
             },
         ];
         let bodyDataa = (num: number = 10) => {
-            let headProps = ['p1', 'p2', 'p6', 'p8', 'p9', 'p10', 'p11', 'p5'];
+            let headProps = ["p1", "p2", "p6", "p8", "p9", "p10", "p11", "p5"];
             let data = [];
-            while (num){
+            while (num) {
                 let o = {};
                 headProps.forEach(prop => {
                     o[prop] = prop + faker.address.cityName();
-                })
+                });
                 data.push(o);
                 num--;
-            };
-            return data
+            }
+            return data;
         };
         let e = new ExportEx();
-        let sheet = e.addSheet('sheet 1', head, bodyDataa());
+        let sheet = e.addSheet("sheet 1", head, bodyDataa());
         sheet.getColumn(1).hidden = true;
+        sheet.getRow(4).hidden = true;
         e.downLoad();
         // let book = new Workbook();
         // let sheet = book.addWorksheet('使用工具导出');
@@ -110,16 +116,13 @@ function App() {
         // book.xlsx.writeBuffer().then(buffer => {
         //     return ExcelTool.downloadBuffer([buffer])
         // });
-
-    }
+    };
 
     return (
         <div>
-            <button >导出--{count}</button>
-            <button >导出多表头</button>
             <button onClick={exportUseTool}>使用工具导出</button>
         </div>
-    )
+    );
 }
 
-render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById("app"));
