1d74589562988d3d9592c03cfb015989865b72f3
zhou_xi_cheng <lkzipsea@163.com>
2022-03-28 16:48:30 +0800

    test: 高度测试

--- html/main.tsx
+++ html/main.tsx
@@ -6,6 +6,7 @@ import { xlsxHeadArr } from "lib/exceljsExtend";
 import { ExportEx } from "lib/exceljsExtend";
 import { faker } from "@faker-js/faker";
 import { headsXNGD, testData } from "./mock/data01";
+import { downloadBuffer } from 'lib/download';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -95,6 +96,8 @@ function App() {
         let sheet = e.addSheet("sheet 1");
         sheet.addRow(["顶部行"]);
         let headInfo = e.addheads(sheet, head); // 添加头
+        e.bodyHeight = 100;
+
         e.addBodyData(bodyData(), sheet, headInfo.bottomHeads);
         // 对第一行添加合并
         sheet.mergeCells(1, 1, 1, headInfo.maxCol);
@@ -105,23 +108,52 @@ function App() {
         sheet.getCell(1, headInfo.maxCol).style.border =
             ExportEx.defaultHeadStyle.border;
 
-        sheet.getRow(1).height = 30;
+        sheet.getRow(1).height = 80;
         e.downLoad();
     };
     const exportHeadTopD = () => {
         let e = new ExportEx();
         let sheet = e.addSheet("sheet 1");
+        sheet.properties.defaultRowHeight = 70;
         let headInfo = e.addheads(sheet, headsXNGD as any); // 添加头
         e.addBodyData(testData, sheet, headInfo.bottomHeads);
         sheet.getColumn(1).hidden = true;
         sheet.getRow(4).hidden = true;
         e.downLoad("hahaha");
     };
+
+    const exportTree = () => {
+        const wb = new Workbook();
+        const ws = wb.addWorksheet('ss');
+        ws.addTable({
+            name: 'MyTable',
+            ref: 'A1',
+            headerRow: true,
+            totalsRow: false,
+            style: {
+                showRowStripes: true,
+            },
+            columns: [
+                {name: 'Date', totalsRowLabel: 'Totals:', filterButton: false},
+                {name: 'Amount', totalsRowFunction: 'sum', filterButton: false},
+            ],
+            rows: [
+                [new Date('2019-07-20'), 70.10],
+                [new Date('2019-07-21'), 70.60],
+                [new Date('2019-07-22'), 70.10],
+            ],
+        });
+        wb.xlsx.writeBuffer().then(buffer => {
+            return downloadBuffer([buffer], 'fileName');
+        });
+    }
+
     return (
         <div>
             <button onClick={exportUseTool}>使用工具导出</button>
             <button onClick={exportHeadTop}>添加头部行信息</button>
             <button onClick={exportHeadTopD}>测试</button>
+            <button onClick={exportTree}>导出树表格</button>
         </div>
     );
 }
