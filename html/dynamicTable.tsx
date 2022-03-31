61aeb32b7ba2b12314461072acf71f245d7176e5
zhou_xi_cheng <lkzipsea@163.com>
2022-03-31 13:50:04 +0800

    test: 修改html测试

--- html/dynamicTable.tsx
+++ html/dynamicTable.tsx
@@ -3,43 +3,48 @@ import { handleColSpan, handleRowSpan, tableHead } from 'lib/tableHeadTool';
 import './dynamicTable.less';
 import { useState } from 'react';
 import { getTreeNodeById } from 'lib/tree';
-import { Workbook } from "exceljs";
-import { ExportEx } from "lib/exceljsExtend";
+import { Workbook } from 'exceljs';
+import { ExportEx } from 'lib/exceljsExtend';
 
 
 const createHeadArr: () => tableHead[] = () => {
     return [
         {
             children: [],
-            prop: 'p1', label: "p1",
+            prop: 'p1', label: 'p1',
             name: 'p1',
         },
         {
-            children: [{prop: 'p3', label: "p3"}, {prop: 'p4', label: "p4"}],
-            prop: 'p7', label: "p7",
+            children: [{prop: 'p3', label: 'p3'}, {prop: 'p4', label: 'p4'}],
+            prop: 'p7', label: 'p7',
         },
         {
             children: [{
-                prop: 'p8A1', label: "p8A1",
+                prop: 'p8A1', label: 'p8A1',
                 children: [{
-                    prop: 'p8A1A1', label: "p8A1A1",
+                    prop: 'p8A1A1', label: 'p8A1A1',
                     children: [{
-                        prop: 'p8A1A1A2', label: "p8A1A1A2",
-                        children: [{prop: 'p8A1A1A2A1', label: "p8A1A1A2A1"}, {prop: 'p8A1A1A2A2', label: "p8A1A1A2A2"}, {prop: 'p8A1A1A2A3', label: "p8A1A1A2A3"}],
-                    }, {prop: 'p8A1A1A3', label: "p8A1A1A3"}],
+                        prop: 'p8A1A1A2', label: 'p8A1A1A2',
+                        children: [
+                            {prop: 'p8A1A1A2A1', label: 'p8A1A1A2A1', show: false}, {
+                            prop: 'p8A1A1A2A2',
+                            label: 'p8A1A1A2A2',
+                            show: false
+                        }, {prop: 'p8A1A1A2A3', label: 'p8A1A1A2A3', show: false}],
+                    }, {prop: 'p8A1A1A3', label: 'p8A1A1A3'}],
                 }, {
-                    prop: 'p8A1A2', label: "p8A1A2"
+                    prop: 'p8A1A2', label: 'p8A1A2'
                 }],
             }, {
-                prop: 'p8A2', label: "p8A2"
+                prop: 'p8A2', label: 'p8A2'
             }],
-            prop: 'p8', label: "p8",
+            prop: 'p8', label: 'p8',
         },
         {
-            prop: 'p9', label: "p9",
+            prop: 'p9', label: 'p9',
             children: [
-                {prop: 'p10', label: "p10"},
-                {prop: 'p11', label: "p11", children: [{prop: 'p12', label: 'p12'}]},
+                {prop: 'p10', label: 'p10'},
+                {prop: 'p11', label: 'p11', children: [{prop: 'p12', label: 'p12'}]},
             ],
         },
     ];
@@ -48,34 +53,33 @@ const createHeadArr: () => tableHead[] = () => {
 const createData = () => {
     return Array.from({length: 20}, (k, v) => {
         return {
-            "p1": "p1" + v,
-            "p3": "p3" + v,
-            "p4": "p4" + v,
-            "p7": "p7" + v,
-            "p8A1": "p8A1" + v,
-            "p8A1A1": "p8A1A1" + v,
-            "p8A1A1A2": "p8A1A1A2" + v,
-            "p8A1A1A2A1": "p8A1A1A2A1" + v,
-            "p8A1A1A2A2": "p8A1A1A2A2" + v,
-            "p8A1A1A2A3": "p8A1A1A2A3" + v,
-            "p8A1A1A3": "p8A1A1A3" + v,
-            "p8A1A2": "p8A1A2" + v,
-            "p8A2": "p8A2" + v,
-            "p8": "p8" + v,
-            "p9": "p9" + v,
-            "p10": "p10" + v,
-            "p11": "p11" + v,
-            "p12": "p12" + v,
+            'p1': 'p1' + v,
+            'p3': 'p3' + v,
+            'p4': 'p4' + v,
+            'p7': 'p7' + v,
+            'p8A1': 'p8A1' + v,
+            'p8A1A1': 'p8A1A1' + v,
+            'p8A1A1A2': 'p8A1A1A2' + v,
+            'p8A1A1A2A1': 'p8A1A1A2A1' + v,
+            'p8A1A1A2A2': 'p8A1A1A2A2' + v,
+            'p8A1A1A2A3': 'p8A1A1A2A3' + v,
+            'p8A1A1A3': 'p8A1A1A3' + v,
+            'p8A1A2': 'p8A1A2' + v,
+            'p8A2': 'p8A2' + v,
+            'p8': 'p8' + v,
+            'p9': 'p9' + v,
+            'p10': 'p10' + v,
+            'p11': 'p11' + v,
+            'p12': 'p12' + v,
             id: 'bData' + v
-        }
-    })
-}
+        };
+    });
+};
 
 export function DynamicTable() {
-    const headsArr = createHeadArr();
     const [headArr, setHeadArr] = useState(createHeadArr());
     const heads: tableHead[][] = [];
-    const dataTbody: any[] = createData()
+    const dataTbody: any[] = createData();
     let deep = 0;
     const bottomHead = [];
     handleColSpan(headArr, {
@@ -103,9 +107,9 @@ export function DynamicTable() {
 
     const exportData = () => {
         const ex = new ExportEx();
-        ex.addSheet('sheet01', headArr, dataTbody)
+        ex.addSheet('sheet01', headArr, dataTbody);
         ex.downLoad();
-    }
+    };
 
     return (
         <div>
@@ -117,12 +121,14 @@ export function DynamicTable() {
                 <button onClick={refresh}>刷新</button>
             </div>
             <table>
-                <col style={{width: '120px'}}/>
-                <col style={{width: '120px'}}/>
-                <col style={{width: '120px'}}/>
-                <col style={{width: '120px'}}/>
-                <col style={{width: '120px'}}/>
-                <col style={{width: '120px'}}/>
+                <colgroup>
+                    <col style={{width: '120px'}}/>
+                    <col style={{width: '120px'}}/>
+                    <col style={{width: '120px'}}/>
+                    <col style={{width: '120px'}}/>
+                    <col style={{width: '120px'}}/>
+                    <col style={{width: '120px'}}/>
+                </colgroup>
                 <thead className="thead">
                 {heads.map((rowHead, index) => {
                     return (<tr key={index + 'hello'}>
@@ -143,7 +149,6 @@ export function DynamicTable() {
                 }
                 </tbody>
             </table>
-
         </div>
     );
 }
@@ -155,7 +160,7 @@ export function ViewXlsx() {
         const reader = new FileReader();
         file.arrayBuffer().then(arrBuffer => {
             const ws = new Workbook();
-            return ws.xlsx.load(arrBuffer)
+            return ws.xlsx.load(arrBuffer);
         }).then(ws => {
             ws.eachSheet((sheet) => {
                 const rows = sheet.getRows(0, sheet.rowCount);
@@ -163,13 +168,13 @@ export function ViewXlsx() {
                 // @ts-ignore
                 console.log(sheet._merges, sheet.getSheetValues());
                 console.log(rows);
-            })
-        })
-        console.log(target.files[0])
-    }
+            });
+        });
+        console.log(target.files[0]);
+    };
     return (
         <div>
             <input type="file" accept="xlsx" onChange={getFile}/>
         </div>
-    )
+    );
 }
