468e44a8d6d5b6dbee15d6618c9d6556288bdd34
fiveroom <lkzipsea@163.com>
2022-10-19 22:18:54 +0800

    break 移除isEquality、downloadBuffer函数

--- src/core.ts
+++ src/core.ts
@@ -1,50 +1,4 @@
 /**
- *
- * 深度对象判断
- *
- * @param a
- * @param b
- * @param noProp
- * @returns
- */
-export function isEquality(a: any, b: any, ...noProp: string[]) {
-    if (a && b) {
-        let typeA = Object.prototype.toString.call(a);
-        let typeB = Object.prototype.toString.call(b);
-        if (typeA == typeB) {
-            switch (typeA) {
-                case '[object Object]':
-                    let keyA = Object.keys(a).filter(i => !noProp.includes(i));
-                    let keyB = Object.keys(b).filter(i => !noProp.includes(i));
-                    if (keyA.length != keyB.length) {
-                        return false;
-                    }
-                    for (let i of keyA) {
-                        if (!isEquality(a[i], b[i], ...noProp)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                case '[object Array]':
-                    if (a.length != b.length) {
-                        return false;
-                    }
-                    for (let i = 0; i < a.length; i++) {
-                        if (!isEquality(a[i], b[i], ...noProp)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                default:
-                    return a == b;
-            }
-        }
-        return false;
-    }
-    return a == b;
-}
-
-/**
  * 链式获取对象
  * @example
  * ```ts
@@ -58,7 +12,7 @@ export function isEquality(a: any, b: any, ...noProp: string[]) {
  * @returns
  */
 export function getDeepObj(data: any, d: string, split = '.') {
-    if (!data || !d) return undefined;
+    if (!data || !d) return;
     let dArr = d.split(split);
     let r = data;
     for (let i = 0; i < dArr.length; i++) {
